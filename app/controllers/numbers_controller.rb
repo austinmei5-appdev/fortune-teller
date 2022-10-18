cfc9c972d56ab8f692ddf6520a3b394cfc5f6f9a
austinmei5 <austinmei@uchicago.edu>
2022-10-18 23:00:45 +0000

    finish lab

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -4,10 +4,28 @@ class NumbersController < ApplicationController
 
     5.times do
       giraffe = rand(1...100)
-      
+
       @zebra.push(giraffe)
     end
 
-    render({ :template => "lottery_stuff/woohoo.html.erb"})
-  end 
+    render({ :template => "lottery_stuff/woohoo.html.erb" })
+  end
+
+  def losers
+    @donkey = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+
+      @donkey.push(giraffe)
+    end
+
+    render({ :template => "lottery_stuff/boohoo.html.erb" })
+  end
+
+  def roll_six
+    @roll = rand (1...6)
+
+    render({ :template => "dice_stuff/roll_six.html.erb" })
+  end
 end
